a906f8840c69bd72d41e32feb19b85e84c7fd427
IvayloStoimenov <46894367+IvayloStoimenov@users.noreply.github.com>
2019-01-23 17:07:58 +0100

    Add files via upload

--- Singularity.hh
+++ Singularity.hh
@@ -6,13 +6,18 @@ MAINTAINER Vanessasaur
 SPECIES Dinosaur
 
 %environment
-RAWR_BASE=/code
+RAWR_BASE=/dia
 export RAWR_BASE
 
 %runscript
 echo "This gets run when you run the image!" 
-exec /bin/bash /ls 
+exec /bin/bash /dia/Singa
 
 %post  
 echo "This section happens once after bootstrap to build the image."  
 ls
+mkdir -p /dia
+ls
+cp Singa /dia/Singa
+cp Singa.specs /dia/Singa.specs
+chmod u+x /dia/Singa
