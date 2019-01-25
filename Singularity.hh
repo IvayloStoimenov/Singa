c5250ae3f23c13cfb4a848f0c562b82a9c8ea023
IvayloStoimenov <46894367+IvayloStoimenov@users.noreply.github.com>
2019-01-25 22:18:30 +0100

    Add files via upload

--- Singularity.hh
+++ Singularity.hh
@@ -18,12 +18,7 @@ echo "This section happens once after bootstrap to build the image."
 apt-get update
 apt-get install -y build-essential
 apt-get install -y wget git curl
-apt-get install -y apt-transport-https
-ls
 mkdir -p /dia
 cd /dia
 git clone https://github.com/IvayloStoimenov/Singa.git
-ls
-cd Singa
-ls
-chmod +x Singa 
+chmod +x ./Singa/Singa 
